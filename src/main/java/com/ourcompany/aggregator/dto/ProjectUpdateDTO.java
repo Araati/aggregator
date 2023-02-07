94890f62491b4943289c4a12814cc0f28af5201b
Soymun <pkekeoke@mail.ru>
2023-02-07 21:12:53 +0300

    Add project test

--- src/main/java/com/ourcompany/aggregator/dto/ProjectUpdateDTO.java
+++ src/main/java/com/ourcompany/aggregator/dto/ProjectUpdateDTO.java
@@ -2,6 +2,7 @@ package com.ourcompany.aggregator.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
@@ -9,6 +10,7 @@ import java.util.Optional;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
 public class ProjectUpdateDTO {
 
     @JsonProperty(value = "description")
@@ -17,12 +19,12 @@ public class ProjectUpdateDTO {
     @JsonProperty(value = "tag_list")
     private List<Long> tagList;
 
-    public Optional<String> getDescription() {
-        return Optional.ofNullable(description);
-    }
-
-    public Optional<List<Long>> getTagList() {
-        return Optional.ofNullable(tagList);
-    }
+//    public Optional<String> getDescription() {
+//        return Optional.ofNullable(description);
+//    }
+//
+//    public Optional<List<Long>> getTagList() {
+//        return Optional.ofNullable(tagList);
+//    }
 
 }
